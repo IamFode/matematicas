52ce766800f01f8dddc8803ace178b71fd9b77be
soyfode <soyfode@gmail.com>
2021-11-15 16:17:01 -0400

    calc est

---
+++ programacion/inf-111/python/tareas_mat/week3/promedios.py
@@ -0,0 +1,20 @@
+
+# función media aritmética, media cuadrática y media aritmética.
+def promedios(lista):
+    sum_arit, sum_cuad, sum_armo = 0,0,0
+    
+    ## loop for para sumar los valores de la lista
+    for i in lista:
+        sum_arit += i
+        sum_cuad += i**2
+        sum_armo += i**(-1)
+    
+    aritmetica = sum_arit / len(lista)
+    cuadratica = (sum_cuad / len(lista))**(1/2)
+    armonica = (sum_armo / len(lista))**(-1)
+    return "Media aritmética: {:.2f} \nMedia cuadrática:{:.2f} \nMedia armónica: {:.2f}".format(aritmetica, cuadratica, armonica)
+
+list_ = list(map(float, input("Agregar lista: ").strip().split()))
+print(promedios(list_))
+
+
