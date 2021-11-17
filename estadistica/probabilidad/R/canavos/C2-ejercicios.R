5222cd7b5f5bc255b574b8aa9ed4b9e6d233ab17
soyfode <soyfode@gmail.com>
2021-11-17 00:01:34 -0400

    est

--- estadistica/probabilidad/R/canavos/C2-ejercicios.R
+++ estadistica/probabilidad/R/canavos/C2-ejercicios.R
@@ -1,1 +1,33 @@
 ########################## EJERCICIOS CAPÍTULO 2 ###############################
+
+####### 2.1 #######
+
+# a) No son mutuamente excluyentes ya que $O\cap M \neq \subset$
+
+# b)
+## 1.
+180/400
+## 2.
+150/400
+## 3.
+30/400
+## 4. P(O\M) = P(O intersección M) / P(M)
+(60/400) / (180/400)
+## 5. P(M\O) = P(M interseccion O) / P(O)
+(60/400) / (200/400)
+
+# c) P(V\H) = P(V) 
+(50/400) / (220/400)
+150/400
+
+# d) P(A\M) = P(A)
+(1/20) / (18/40)
+18/40
+
+# e)
+## 1. P(A union M) = P(A) + P(M) -P(A inters M)
+50/400 + 180/400 - 20/400
+## 2. P(A union com(M)) = P(A) + P(H) - P(A intes H)
+50/400 + 180/400 - 30/400
+## 3. conjunto vacio
+## 4. P(M\A) = P(M inters A) / P(A)
