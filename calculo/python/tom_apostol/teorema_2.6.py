e1c6e66c5732cfc93d3b115955786bb52c66c47b
soyfode <soyfode@gmail.com>
2021-12-21 17:08:54 -0400

    tom apostol

--- calculo/python/tom_apostol/teorema_2.6.py
+++ calculo/python/tom_apostol/teorema_2.6.py
@@ -1,12 +1,29 @@
 import numpy as np
+import sympy as sp
 
-def teo2_6(a,b):
-    if 0 <= b-a and b-a <= 2*np.pi:
+def teo2_6(a,b,f):
+    sum1, sum2, n = 0, 0, 1000000
+
+    f = sp.Symbol('f')
+
+    for k in range(0,n-1):
+        x = a + k*(b-a)/n
+        sum1 += f 
+        res1 = (b-a)/n * sum1
+
+    for k in range(1,n):
+        x1 = a + k*(b-a)/n # genera los puntos de x 
+        sum2 += f 
+        res2 = (b-a)/n * sum2
+
+    print("total menor: {}\nTotal mayor: {}".format(res1, res2))
 
 
+a = float(input("Ingrese a: "))
+b = float(input("Ingrese b: "))
+func = input()
 
-a = float(input("a: "))
-b = float(input("b: "))
+teo2_6(a,b,func)
 
-print(teo2_6(a,b))
+
 
