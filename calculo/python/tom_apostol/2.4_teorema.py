1aa8317ab3b2304e56b8037e50d3168d001ef371
soyfode <soyfode@gmail.com>
2022-06-28 21:20:42 -0400

    calc

--- calculo/python/tom_apostol/2.4_teorema.py
+++ calculo/python/tom_apostol/2.4_teorema.py
@@ -1,20 +1,19 @@
-
 import numpy as np
 
 a = float(input("introducir a: "))
 
-sum1 = 0
-sum2 = 0
+sum1, sum2 = 0, 0
+
+def f(x):
+    return np.cos(x)
 
 n = 1000000
 
-for k in range(0,n-1):
-    x =  k*a/n
-    sum1 += np.cos(x) 
+for k in range(n-1):
+    x, x1 = k*a/n, (k+1)*a/n
+    sum1 += f(x)
+    sum2 += f(x1)
 
-for k in range(1,n,1):
-    x1 =  k*a/n
-    sum2 += np.cos(x1) 
 
 print("total menor: {}".format((a/n)*sum1))
 print("total mayor: {}".format((a/n)*sum2))
--- calculo/python/tom_apostol/teorema1_13.py
+++ calculo/python/tom_apostol/teorema1_13.py
@@ -3,13 +3,12 @@ import numpy as np
 def teo1_13(a,b):
     sum1, sum2, n = 0, 0, 1000000
 
-    for k in range(0,n-1):
-        x = a + k*(b-a)/n
-        sum1 += x**2
+    f = lambda x: x**2
 
-    for k in range(1,n):
-        x1 = a + k*(b-a)/n
-        sum2 += x1**2
+    for k in range(n-1):
+        x, x1 = a + k*a/n, a + (k+1)*a/n
+        sum1 += f(x)
+        sum2 += f(x1)
 
     print("total menor: {}\nTotal mayor: {}".format(((b-a)/n)*sum1,((b-a)/n)*sum2))
 
@@ -17,3 +16,4 @@ a = float(input("Ingrese a: "))
 b = float(input("Ingrese b: "))
 
 teo1_13(a,b)
+
