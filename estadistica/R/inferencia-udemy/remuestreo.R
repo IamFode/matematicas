60f251658ac8436a9b73176f960c744c8b6f3456
soyfode <soyfode@gmail.com>
2022-08-30 17:12:32 -0400

    calc est

---
+++ estadistica/R/inferencia-udemy/remuestreo.R
@@ -0,0 +1,16 @@
+
+######################## Técnicas de Remuestreo ################################
+
+"
+1. Remuestrar la muestra: Tomar muchas muestras aleatorias simples de la muestras
+de  las que disponemos, cada uan de ellas del mismo tamaño que la muestra original
+(con resposición)
+
+2. Calcular el estimador sobre cada una de estas submuestras.
+
+3. Organizar los resultados en un vector.
+
+4. Usar este vector para calcular un intervalo de confianza.
+
+"
+
