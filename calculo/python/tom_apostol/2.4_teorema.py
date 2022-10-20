b685db131a7c2426289bb19717933dc32fd79f94
soyfode <soyfode@gmail.com>
2022-10-20 15:59:51 -0400

    calc lineal

--- calculo/python/tom_apostol/2.4_teorema.py
+++ calculo/python/tom_apostol/2.4_teorema.py
@@ -2,18 +2,31 @@ import numpy as np
 
 a = float(input("introducir a: "))
 
-sum1, sum2 = 0, 0
-
-def f(x):
-    return np.cos(x)
-
 n = 1000000
 
-for k in range(n-1):
-    x, x1 = k*a/n, (k+1)*a/n
-    sum1 += f(x)
-    sum2 += f(x1)
+if 0 < a and a < 1/2*np.pi and n>=1:
+
+    def f(x):
+        return np.cos(x)
+
+    sum1, sum2 = 0, 0
+
+    for k in range(n-1):
+        x, x1 = k*a/n, (k+1)*a/n
+        sum1 += f(x)
+        sum2 += f(x1)
+
+    print("total menor: {}".format((a/n)*sum1))
+    print("total mayor: {}".format((a/n)*sum2))
+    print(a/n*s)
+
+else:
+
+    print("a debe ser mayor que 0 y menor que pi/2 o n debe ser mayor o igual a 1")
 
 
-print("total menor: {}".format((a/n)*sum1))
-print("total mayor: {}".format((a/n)*sum2))
+
+
+
+
+
