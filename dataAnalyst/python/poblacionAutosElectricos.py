809fc4f4bd615ccf25bc1804c94ec762614f785e
fode <soyfode@gmail.com>
2023-01-29 22:58:02 -0400

    calc lineal

--- dataAnalyst/python/poblacionAutosElectricos.py
+++ dataAnalyst/python/poblacionAutosElectricos.py
@@ -9,10 +9,13 @@ df = pd.read_csv('/media/fode/PRO TOOLS/dataSets/Electric_Vehicle_Population_Dat
 """
 # print first 5 rows
 print(df.head())
+
 # shape of data
 print(df.shape)
+
 # column names list of data 
 print(df.columns.tolist())
+
 # summary of data
 print(df.describe())
 """
@@ -24,10 +27,30 @@ countModelYear.plot(kind="bar", title="Count of Model Year")
 plt.show()
 """
 
+"""
 df1 = df[["Model Year","Make","Model"]]
 
 model = df1.pivot_table(values = "Model", index=["Make"],columns="Model Year", aggfunc="count",fill_value="-",margins=True)
-print(model.sort_values("All",ascending=True))
+timeCar = model.sort_values("All",ascending=True)
+
+# groupby model and make
+groupModel = df.groupby(["Make","Model"])["Model"].count()
+"""
+
+dfgeo = df[["Make","Vehicle Location"]]
+
+print(dfgeo["Vehicle Location"].head())
+
+
+"""
+dfgeo.plot(kind="scatter", x="Make", y="Vehicle Location", title="Make and Vehicle Location")
+plt.show()
+"""
+
+
+
+
+
 
 
 
